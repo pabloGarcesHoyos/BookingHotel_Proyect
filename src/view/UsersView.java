4f17461855719048007fe41071efc1aab7eadc06
pablo <pablogarceshoyos@gmail.com>
2024-02-24 16:55:34 -0500

    feat: new login users

--- src/view/UsersView.java
+++ src/view/UsersView.java
@@ -22,6 +22,7 @@ public class UsersView extends javax.swing.JFrame {
      */
     public UsersView() throws SQLException {
         initComponents();
+        setLocationRelativeTo(null);
 
         controller = new UserController();
     }
@@ -217,9 +218,7 @@ public class UsersView extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
